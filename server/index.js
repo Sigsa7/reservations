16c3b8e1d3ae8c6e428997e1a4ae5411e4bb635a
Katherine Wang <kwang004@gmail.com>
2019-07-30 11:57:43 -0700

    Set up sserver endpoints

--- server/index.js
+++ server/index.js
@@ -1,7 +1,30 @@
-const app = require('./app');
+const express = require('express');
 
-const PORT = 3002;
+const app = express();
 
-module.exports = app.listen(PORT, () => {
-  console.log(`listening on port ${PORT}...`);
+const port = 3005;
+
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+
+app.get('/booking/reserved/:restaurantID', (req, res) => {
+  // return unavailable times for booking
 });
+
+app.get('/booking/count/:restaurantID', (req, res) => {
+  // return daily booking count
+});
+
+app.post('/booking/:restaurantID', (req, res) => {
+  // create a new reservations
+});
+
+app.put('/booking/:reservationID', (req, res) => {
+  // update an existing reservation
+});
+
+app.delete('/booking/:reservationID', (req, res) => {
+  // delete an existing reservation
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
