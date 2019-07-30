4acc0ce78bae42bbd0f2600940923c78e941f12d
Katherine Wang <kwang004@gmail.com>
2019-07-30 15:18:50 -0700

    Add queries for bookingCount and newReservation

--- server/index.js
+++ server/index.js
@@ -1,17 +1,50 @@
+/* eslint-disable no-nested-ternary */
 const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const { Pool } = require('pg');
+const moment = require('moment');
+
 const app = express();
+
+const pool = new Pool({
+  database: 'sigsa_reservations2',
+});
+
 const port = 3005;
 
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+
 app.get('/booking/reserved/:restaurantID', (req, res) => {
-  // return unavailable times for booking
+
 });
 
 app.get('/booking/count/:restaurantID', (req, res) => {
-  // return daily booking count for restaurant
+  const { restaurantID } = req.params;
+  const { date } = req.body;
+
+  pool.query('SELECT COUNT(*) FROM reservations WHERE restaurant_id = $1 AND create_at = $2', [restaurantID, date], (err, data) => {
+    if (err) {
+      throw err;
+    }
+    res.send(data.rows[0]);
+  });
 });
 
 app.post('/booking/:restaurantID', (req, res) => {
-  // create a new reservations
+  const { restaurantID } = req.params;
+  const { dateTime, partySize } = req.body;
+  const tableSize = partySize > 10 ? 20 : (partySize % 2 === 0 ? partySize : partySize + 1);
+  const createAt = moment().format('YYYY-MM-DD');
+  
+  pool.query('INSERT INTO reservations (restaurant_id, date_time, party_size, table_size, create_at) VALUES($1, $2, $3, $4, $5)',
+    [restaurantID, dateTime, partySize, tableSize, createAt], (err, data) => {
+      if (err) {
+        throw err;
+      }
+      res.sendStatus(200);
+    });
 });
 
 app.put('/booking/:reservationID', (req, res) => {
