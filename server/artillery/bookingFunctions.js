64c6ced56328527f519344c96b3c56df38d753cc
Katherine Wang <kwang004@gmail.com>
2019-07-31 17:03:51 -0700

    Install artillery and new relic for load testing

---
+++ server/artillery/bookingFunctions.js
@@ -0,0 +1,44 @@
+'use strict';
+
+module.exports = {
+  createReservation,
+  checkReserved
+};
+
+const faker = require('faker');
+const moment = require('moment');
+const minuteOptions = ['15', '30', '45', '00'];
+
+function checkReserved(userContext, events, done) {
+  // generate data with Faker:
+  const restaurantID = faker.random.number({ min: 1500000, max: 2000000 });
+  const partySize = faker.random.number({ min: 1, max: 20 });
+  const date = moment.utc().add(faker.random.number({ min: 1, max: 90 }), 'days').format('YYYY-MM-DD');
+  const time = `${Math.ceil(Math.random() * 23)}:${minuteOptions[Math.floor(Math.random() * 4)]}:00`;
+  const dateTime = `${date} ${time}`;
+  // add variables to virtual user's context:
+  userContext.vars.partySize = partySize;
+  userContext.vars.dateTime = dateTime;
+  userContext.vars.restaurantID = restaurantID;
+  // continue with executing the scenario:
+  return done();
+}
+
+function createReservation(userContext, events, done) {
+  // generate restaurant ID
+  const restaurantID = faker.random.number({ min: 1500000, max: 2000000 });
+  
+  // generate date
+  const date = moment.utc().add(faker.random.number({ min: 1, max: 90 }), 'days').format('YYYY-MM-DD');
+  const time = `${Math.ceil(Math.random() * 23)}:${minuteOptions[Math.floor(Math.random() * 4)]}:00`;
+  const dateTime = `${date} ${time}`;
+
+  const partySize = faker.random.number({ min: 1, max: 20 });
+  
+  // add variables to virtual user's context:
+  userContext.vars.dateTime = dateTime;
+  userContext.vars.restaurantID = restaurantID;
+  userContext.vars.partySize = partySize;
+  // continue with executing the scenario:
+  return done();
+}
--- server/index.js
+++ server/index.js
@@ -1,9 +1,12 @@
 /* eslint-disable no-nested-ternary */
+const newRelic = require('newrelic');
 const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const { Pool } = require('pg');
 const moment = require('moment');
+const path = require('path');
+const expressStaticGzip = require('express-static-gzip');
 
 const app = express();
 
@@ -16,6 +19,17 @@ const port = 3005;
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 
+app.use(express.static(path.join(__dirname, '/../public/')));
+app.use('/:restaurant_id', express.static(path.join(__dirname, '/../public/')));
+app.use('/', expressStaticGzip('/../public/', {
+  enableBrotli: true,
+    orderPreference: ['br', 'gz'],
+    setHeaders: function (res, path) {
+      res.setHeader("Cache-Control", "public, max-age=31536000");
+    }
+  })
+);
+
 app.get('/booking/reserved/:restaurantID', (req, res) => {
   const { restaurantID } = req.params;
   let { dateTime } = req.body;
@@ -25,20 +39,26 @@ app.get('/booking/reserved/:restaurantID', (req, res) => {
   const start = moment(dateTime).clone().subtract(2, 'hours').format('YYYY-MM-DD HH:mm');
   const end = moment(dateTime).clone().add(2, 'hours').format('YYYY-MM-DD HH:mm');
   let number;
-
   pool.query('SELECT number_of_tables FROM seating WHERE restaurant_id = $1 AND table_size = $2',
     [restaurantID, tableSize], (err, data) => {
-      if (err) throw err;
-      number = data.rows[0].number_of_tables;
-
-      pool.query('SELECT date_time, count(*) FROM reservations WHERE restaurant_id = $1 AND date_time >= $2 AND date_time <= $3 AND table_size = $4 GROUP BY date_time',
-        [restaurantID, start, end, tableSize], (err, rezData) => {
-          if (err) throw err;
-
-          const filtered = rezData.rows.filter(element => number - element.count < 1);
-          const reserved = filtered.map(element => element.date_time);
-          res.send(reserved);
-        });
+      if (err) {
+        console.log(err);
+        res.sendStatus(500);
+      } else {
+        number = data.rows.length === 0 ? 0 : data.rows[0].number_of_tables;
+  
+        pool.query('SELECT date_time, count(*) FROM reservations WHERE restaurant_id = $1 AND date_time >= $2 AND date_time <= $3 AND table_size = $4 GROUP BY date_time',
+          [restaurantID, start, end, tableSize], (err, rezData) => {
+            if (err) {
+              console.log(err);
+              res.sendStatus(500);
+            } else {
+              const filtered = rezData.rows.filter(element => number - element.count < 1);
+              const reserved = filtered.map(element => element.date_time);
+              res.send(reserved);
+            }
+          });
+      }
     });
 });
 
@@ -48,7 +68,7 @@ app.get('/booking/count/:restaurantID', (req, res) => {
 
   pool.query('SELECT COUNT(*) FROM reservations WHERE restaurant_id = $1 AND create_at = $2', [restaurantID, date], (err, data) => {
     if (err) {
-      throw err;
+      res.sendStatus(500);
     }
     res.send(data.rows[0]);
   });
@@ -63,9 +83,10 @@ app.post('/booking/:restaurantID', (req, res) => {
   pool.query('INSERT INTO reservations (restaurant_id, date_time, party_size, table_size, create_at) VALUES($1, $2, $3, $4, $5)',
     [restaurantID, dateTime, partySize, tableSize, createAt], (err, data) => {
       if (err) {
-        throw err;
+        res.sendStatus(500);
+      } else {
+        res.sendStatus(200);
       }
-      res.sendStatus(200);
     });
 });
 
@@ -76,7 +97,9 @@ app.put('/booking/:reservationID', (req, res) => {
 
   pool.query('UPDATE reservations SET date_time = $1, party_size = $2, table_size = $3 WHERE id = $4',
     [dateTime, partySize, tableSize, reservationID], (err, data) => {
-      if (err) throw err;
+      if (err) {
+        res.sendStatus(500);
+      }
       res.sendStatus(200);
     });
 });
@@ -85,7 +108,9 @@ app.delete('/booking/:reservationID', (req, res) => {
   const { reservationID } = req.params;
 
   pool.query('DELETE FROM reservations where id = $1', [reservationID], (err, data) => {
-    if (err) throw err;
+    if (err) {
+      res.sendStatus(500);
+    }
     res.sendStatus(200);
   });
 });
