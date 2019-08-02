12af46362724bf5fe26f8f42ed7408cabeea942d
Katherine Wang <kwang004@gmail.com>
2019-08-02 15:04:43 -0700

    Update caching functions to include caching for initial table query

--- database/controllers/dbControllers.js
+++ database/controllers/dbControllers.js
@@ -8,38 +8,76 @@ client.on('connect', function() {
   console.log('connected');
 });
 
+const pool = new Pool({
+  database: 'sigsa_reservations2',
+});
+
+
 const cacheReservedDates = (req, res, next) => {
   const { restaurantID } = req.params;
   const { dateTime, partySize } = req.body;
-  const key = `${restaurantID}-${dateTime}-${partySize}`;
-  console.log('key in cache function', key);
-  client.get(key, (err, data) => {
+  const tableSize = partySize > 10 ? 20 : (partySize % 2 === 0 ? partySize : partySize + 1);
+  const reservationKey = `${restaurantID}-${dateTime}-${tableSize}`;
+  client.get(reservationKey, (err, data) => {
     if (err) throw err;
 
     if (data !== null) {
-      console.log('caching works! sending data...');
       res.send(JSON.parse(data));
     } else {
-      console.log('moving to next function');
       next();
     }
   });
 };
 
-const pool = new Pool({
-  database: 'sigsa_reservations2',
-});
+const cacheTables = (req, res, next) => {
+  const { restaurantID } = req.params;
+  let { dateTime } = req.body;
+  const { partySize } = req.body;
+  const tableSize = partySize > 10 ? 20 : (partySize % 2 === 0 ? partySize : partySize + 1);
+  const tableKey = `${restaurantID}-${tableSize}`;
+  const reservationKey = `${restaurantID}-${dateTime}-${tableSize}`;
+  dateTime = moment.utc(dateTime).format('YYYY-MM-DD HH:mm');
+  const start = moment.utc(dateTime).clone().subtract(2, 'hours').format('YYYY-MM-DD HH:mm');
+  const end = moment.utc(dateTime).clone().add(2, 'hours').format('YYYY-MM-DD HH:mm');
+
+  client.get(tableKey, (err, data) => {
+    if (err) throw err;
+
+    if (data !== null) {
+      // means you have access to the number of tables, so get the reserved dates, set it, and send the response
+      const number = Number(data);
+
+      pool.query('SELECT date_time, count(*) FROM reservations WHERE restaurant_id = $1 AND date_time >= $2 AND date_time <= $3 AND table_size = $4 GROUP BY date_time',
+        [restaurantID, start, end, tableSize], (err, rezData) => {
+          if (err) {
+            console.log(err);
+            res.sendStatus(500);
+          } else {
+            const filtered = rezData.rows.filter(element => number - element.count < 1);
+            const reserved = filtered.map(element => element.date_time);
+            client.set(reservationKey, JSON.stringify(reserved));
+            res.send(reserved);
+          }
+        });
+    } else {
+      // if you don't have the number of tables, need to getTables and perform the entire query
+      next();
+    }
+  });
+};
 
 const getReservedDates = (req, res) => {
   const { restaurantID } = req.params;
   let { dateTime } = req.body;
   const { partySize } = req.body;
-  const key = `${restaurantID}-${dateTime}-${partySize}`;
   const tableSize = partySize > 10 ? 20 : (partySize % 2 === 0 ? partySize : partySize + 1);
-  dateTime = moment(dateTime).format('YYYY-MM-DD HH:mm');
-  const start = moment(dateTime).clone().subtract(2, 'hours').format('YYYY-MM-DD HH:mm');
-  const end = moment(dateTime).clone().add(2, 'hours').format('YYYY-MM-DD HH:mm');
+  const tableKey = `${restaurantID}-${tableSize}`;
+  const reservationKey = `${restaurantID}-${dateTime}-${tableSize}`;
+  dateTime = moment.utc(dateTime).format('YYYY-MM-DD HH:mm');
+  const start = moment.utc(dateTime).clone().subtract(2, 'hours').format('YYYY-MM-DD HH:mm');
+  const end = moment.utc(dateTime).clone().add(2, 'hours').format('YYYY-MM-DD HH:mm');
   let number;
+
   pool.query('SELECT number_of_tables FROM seating WHERE restaurant_id = $1 AND table_size = $2',
     [restaurantID, tableSize], (err, data) => {
       if (err) {
@@ -47,6 +85,7 @@ const getReservedDates = (req, res) => {
         res.sendStatus(500);
       } else {
         number = data.rows.length === 0 ? 0 : data.rows[0].number_of_tables;
+        client.set(tableKey, number);
 
         pool.query('SELECT date_time, count(*) FROM reservations WHERE restaurant_id = $1 AND date_time >= $2 AND date_time <= $3 AND table_size = $4 GROUP BY date_time',
           [restaurantID, start, end, tableSize], (err, rezData) => {
@@ -56,8 +95,7 @@ const getReservedDates = (req, res) => {
             } else {
               const filtered = rezData.rows.filter(element => number - element.count < 1);
               const reserved = filtered.map(element => element.date_time);
-              console.log('key in actual function', key);
-              client.set(key, JSON.stringify(reserved));
+              client.set(reservationKey, JSON.stringify(reserved));
               res.send(reserved);
             }
           });
@@ -81,7 +119,7 @@ const createReservation = (req, res) => {
   const { restaurantID } = req.params;
   const { dateTime, partySize } = req.body;
   const tableSize = partySize > 10 ? 20 : (partySize % 2 === 0 ? partySize : partySize + 1);
-  const createAt = moment().format('YYYY-MM-DD');
+  const createAt = moment.utc().format('YYYY-MM-DD');
 
   pool.query('INSERT INTO reservations (restaurant_id, date_time, party_size, table_size, create_at) VALUES($1, $2, $3, $4, $5)',
     [restaurantID, dateTime, partySize, tableSize, createAt], (err, data) => {
@@ -125,4 +163,5 @@ module.exports = {
   updateReservation,
   deleteReservation,
   cacheReservedDates,
+  cacheTables,
 };
--- database/index.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const db = mongoose.connection;
-
-mongoose.connect('mongodb://mongo:27017/reservation_module', {useNewUrlParser: true});
-
-db.on('error', console.error.bind(console, 'connection error:'));
-
-module.exports.db = db;
